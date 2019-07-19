c27d205f8b27fd9e651fa62722a7de44a9087b10
roXieTar <43473992+roXieTar@users.noreply.github.com>
2019-07-19 14:41:51 +0800

    Update TamperHack.js

--- TamperHack.js
+++ TamperHack.js
@@ -1,9 +1,9 @@
-document.addEventListener('click', function(element) {
-   if (element.target.title) {
+document.addEventListener('click', function(event) {
+   if (event.target.title) {
       window.setTimeout(function() {
          require(['function-widget-1:share/util/shareFriend/createLinkShare.js']).prototype.makePrivatePassword = () => {
             return prompt('请输入自定义密码', '1234');
          }
       }, 500);
    }
-}, false);
+}, false)
