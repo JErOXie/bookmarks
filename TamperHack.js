c2dfcecade7a5b64c7d114f12e0e947a7b42b06b
roXieTar <43473992+roXieTar@users.noreply.github.com>
2019-07-19 14:05:30 +0800

    Update and rename baiduPan.js to TamperHack.js

--- TamperHack.js
+++ TamperHack.js
@@ -1,5 +1,5 @@
-document.addEventListener('click', function(butron) {
-   if (butron.target.title == '分享') {
+document.addEventListener('click', function(element) {
+   if (element.target.title) {
       window.setTimeout(function() {
          require(['function-widget-1:share/util/shareFriend/createLinkShare.js']).prototype.makePrivatePassword = () => {
             return prompt('请输入自定义密码', '1234');
