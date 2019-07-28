32044d8ae1906c0393fb16859d766dd30b9f51c8
roXieTar <43473992+roXieTar@users.noreply.github.com>
2019-07-29 04:06:29 +0800

    Update rollerText.js

--- rollerText.js
+++ rollerText.js
@@ -1,29 +1,3 @@
-
-const handleOrientation = event => {
-
-  const x = event.beta;
-
-
-
-  if (x < 40 && x > 20) return;
-
-
-
-  if (x > 40 && x <= 60) {
-
-    window.scrollBy(0, 1);
-
-  } else if (x > 60) {
-
-    window.scrollBy(0, 2);
-
-  } else {
-
-    window.scrollBy(0, -1);
-
-  }
-
-};
-
-
-window.addEventListener("deviceorientation", handleOrientation)
+function handleOrientation(ele) {
+   if (ele.beta > 40) window.scrollBy(0, 2);
+}; window.addEventListener('deviceorientation', handleOrientation)
