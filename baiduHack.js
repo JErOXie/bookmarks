0da9bf8b7f3a86e177137960eb57581ef096ff89
roXieTar <43473992+roXieTar@users.noreply.github.com>
2019-07-29 04:30:54 +0800

    Update baiduHack.js

--- baiduHack.js
+++ baiduHack.js
@@ -1,7 +1,7 @@
-document.addEventListener('click',function(event) {
+document.addEventListener('click',function() {
    window.setTimeout(function() {
       require(['function-widget-1:share/util/shareFriend/createLinkShare.js']).prototype.makePrivatePassword = () => {
          return prompt('请输入自定义密码', '1234');
       }
    }, 1000);
-}, true)
+})
