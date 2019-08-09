661da84706f7784247ec3a5911c126f034ebba99
roXieTar <43473992+roXieTar@users.noreply.github.com>
2019-08-09 22:53:40 +0800

    Delete normalize-fonts.js

--- normalize-fonts.js
+++
@@ -1,6 +0,0 @@
-javascript:(function() {
-	if (document.styleSheets.length) {
-		var x = document.styleSheets[document.styleSheets.length - 1];
-		x.insertRule('* { font-family: -apple-system, BlinkMacSystemFont, PingFang, sanserif !important; }', x.cssRules.length);
-	}
-})();
